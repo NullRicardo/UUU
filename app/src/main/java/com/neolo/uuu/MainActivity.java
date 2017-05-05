c30f2ea4882102aade40bb7c8a8840da351a3d22
Li Qinglin <871001110@qq.com>
2017-05-05 10:41:54 +0800

    wode assda

--- app/src/main/java/com/neolo/uuu/MainActivity.java
+++ app/src/main/java/com/neolo/uuu/MainActivity.java
@@ -2,12 +2,15 @@ package com.neolo.uuu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button)findViewById(R.id.all).saveHierarchyState(setSupportActionBar(savedInstanceState);
     }
 }
